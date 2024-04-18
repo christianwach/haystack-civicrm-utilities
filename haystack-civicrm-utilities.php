cbb9779fab99a445d5a1062aa8e96b6cfd018e20
Christian Wach <needle@haystack.co.uk>
2024-04-18 11:12:44 +0100

    Bump version to 1.0.0

--- haystack-civicrm-utilities.php
+++ haystack-civicrm-utilities.php
@@ -6,7 +6,7 @@
  * Description:       Provides additional utilities for working with CiviCRM.
  * Plugin URI:        https://github.com/christianwach/haystack-civicrm-utilities
  * GitHub Plugin URI: https://github.com/christianwach/haystack-civicrm-utilities
- * Version:           1.0.0b
+ * Version:           1.0.0
  * Author:            Christian Wach
  * Author URI:        https://haystack.co.uk
  * Text Domain:       haystack-civicrm-utilities
@@ -20,7 +20,7 @@
 defined( 'ABSPATH' ) || exit;
 
 // Version.
-define( 'HAYSTACK_CU_VERSION', '1.0.0b' );
+define( 'HAYSTACK_CU_VERSION', '1.0.0' );
 
 // Main plugin file.
 if ( ! defined( 'HAYSTACK_CU_FILE' ) ) {
